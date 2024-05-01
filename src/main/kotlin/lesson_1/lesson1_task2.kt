68e771c59ef53d4fc1f14f3749f34b542aca07e9
user <amislaurov@yandex.ru>
2024-05-01 20:01:58 +0300

    KS-1-2 change variables

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_1
+
+val orderQuantity:Int = 75
+val textForOrder:String = "Thank you for ordered"
+val workers:Int = 2000 - 1
+
+fun main(){
+    println("${orderQuantity} orders")
+    println(textForOrder)
+//    println(workers)
+    println(workers)
+}
