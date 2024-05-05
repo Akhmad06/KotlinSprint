3b08a922f7bfb00031a07664213620141a36e0bf
user <amislaurov@yandex.ru>
2024-05-05 21:32:33 +0300

    KS-1-3 fly to space. Create and change variables

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_1
+
+fun main(){
+    val year:Int = 1961
+    var hour: String = "09"
+    var minute: String = "07"
+
+    println(year)
+    println(hour)
+    println(minute)
+
+    print(landing)
+}
+
+    var hour:Int = 10
+    var minute:Int = 55
+    val landing = "$hour:$minute"
